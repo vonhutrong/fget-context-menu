83fbf9df1dcbc9cf76fab169208e9715f814834e
TrongVo <Trong.Vo@mgm-tp.com>
2018-08-12 10:44:45 +0700

    update code and add notification when user using with unsupported link

--- background.js
+++ background.js
@@ -1,31 +1,56 @@
-var fshareLinkPrefix = "fshare.vn/file/";
+var fshareFileLinkPrefix = "fshare.vn/file/";
+var fshareFileIdLength = 12;
 var menuTitle = "Get link";
 
 function getDecodedURLRecursively(url) {
-    if(url.indexOf('%') != -1) {
+    if (url.indexOf('%') != -1) {
         return getDecodedURLRecursively(decodeURIComponent(url));
     }
     return url;
 }
 
+function notify(title, message) {
+    var opt = {
+        type: "basic",
+        title: title,
+        message: message,
+        iconUrl: "/images/cloud.png"
+    };
+    chrome.notifications.create(opt);
+}
+
+function getFshareLink(decodedLink) {
+    return decodedLink.split(fshareFileLinkPrefix)[1];
+}
+
+function getFshareFileId(decodedLink) {
+    return getFshareLink(decodedLink).substring(0, fshareFileIdLength);
+}
+
+function isRightClickOnFshareFileLink(info, decodedLink) {
+    return info.menuItemId === getLinkContexMenuId && decodedLink.includes(fshareFileLinkPrefix);
+}
+
 function genericOnClick(info, tab) {
-  console.debug("item was clicked", info.menuItemId);
-  console.debug("info",JSON.stringify(info));
-  console.debug("tab", JSON.stringify(tab));
+    console.debug("item was clicked", info.menuItemId);
+    console.debug("info", JSON.stringify(info));
+    console.debug("tab", JSON.stringify(tab));
 
-  var decodedLink = getDecodedURLRecursively(info.linkUrl);
+    var decodedLink = getDecodedURLRecursively(info.linkUrl);
 
-  if (info.menuItemId === getLinkContexMenuId && decodedLink.includes(fshareLinkPrefix)) {
+    if (isRightClickOnFshareFileLink(info, decodedLink)) {
 
-      console.debug("User has right clicked on a fshare link");
+        console.debug("User has right clicked on a fshare link");
 
-      var fshareFileId = decodedLink.split(fshareLinkPrefix)[1];
-      console.debug("File id", fshareFileId);
+        var fshareFileId = getFshareFileId(decodedLink);
+        console.debug("File id", fshareFileId);
 
-      var link = "https://getlinkfshare.com/file/" + fshareFileId;
-      window.open(link, '_blank');
-      console.info("Opened page", link);
-  }
+        var link = "https://getlinkfshare.com/file/" + fshareFileId;
+        window.open(link, '_blank');
+        console.info("Opened page", link);
+    } else {
+        notify("Unsupported link", "You must choose a fshare link of a file");
+    }
 }
 
 var getLinkContexMenuId = chrome.contextMenus.create({
